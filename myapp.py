58759d82add0577030fc1cf68d63378ba2c80fc3
Bagalisk <savethemnone@gmail.com>
2013-05-17 12:56:35 +0800

    學生代號: no -> id

--- myapp.py
+++ myapp.py
@@ -21,14 +21,14 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
 class StudentDetail(db.Model):
-    no = db.IntegerProperty()
+    id = db.StringProperty()
     date = db.StringProperty()
     exams = db.StringProperty(multiline=True)
     daily_notes = db.StringProperty(multiline=True)
     contact_matters = db.StringProperty(multiline=True)
 
 class Student(db.Model):
-    no = db.IntegerProperty()
+    id = db.StringProperty()
     birthday = db.StringProperty()
     name = db.StringProperty()
     address = db.StringProperty()
@@ -46,6 +46,25 @@ class Student(db.Model):
     contact_matters = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
 
+class StudentDetailHandler(webapp2.RequestHandler):
+    def post(self):
+        q = self.request
+        r = self.response
+        s = Student(key_name=q.get('id','0'))
+        sd = StudentDetail.gql('where id=:1 and date=:2',q.get('id','0'),q.get('date','2013/01/01')).get()
+        sd.id = q.get('id','0')
+        sd.date = q.get('date','2013/01/01')
+        sd.exams = q.get('exams','')
+        sd.daily_notes = q.get('daily_notes','')
+        sd.contact_matters = q.get('contact_matters','')
+        sd.put()
+        s = Student.gql('where id =:1',q.get('id','0')).get()
+        s.exams = q.get('exams','')
+        s.daily_notes = q.get('daily_notes','')
+        s.contact_matters = q.get('contact_matters','')
+        s.put()
+        
+        self.redirect('/students')
 
 class StudentsHandler(webapp2.RequestHandler):
     def get(self):
@@ -67,10 +86,13 @@ class StudentHandler(webapp2.RequestHandler):
     def get(self):
         q = self.request
         r = self.response
-        s = Student.gql('where no =:1',int(q.get('no','0'))).get()
+        s = Student.gql('where id =:1',q.get('id','0')).get()
+            
+        student_details = StudentDetail.gql('where id =:1 order by date desc',q.get('id','0')).fetch(100)
         
         template_values = { }
         template_values['s'] = s
+        template_values['student_details'] = student_details
 
         request = urllib2.unquote(self.request.path)
         path = os.path.join(os.path.dirname(__file__), 'student.html')
@@ -79,8 +101,8 @@ class StudentHandler(webapp2.RequestHandler):
     def post(self):
         q = self.request
         r = self.response
-        s = Student(key_name=q.get('no','0'))
-        s.no = int(q.get('no','0'))
+        s = Student(key_name=q.get('id','0'))
+        s.id = q.get('id','0')
         s.name = q.get('name','')
         s.birthday = q.get('birthday','')
         s.address = q.get('address','')
@@ -139,5 +161,6 @@ app = webapp2.WSGIApplication([
     , ('/login', LoginHandler)
     , ('/students', StudentsHandler)
     , ('/student', StudentHandler)
+    , ('/student_detail', StudentDetailHandler)
     , ('/.*', CommandHandler)
 ], debug=True)
